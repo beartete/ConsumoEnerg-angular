1c8df3f5ff2b66cf0690b5f34fb77f88eadac460
beartete <isabellacristinavieira2003@gmail.com>
2023-04-16 12:26:07 -0300

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsumoEnergiaComponent } from './consumo-energia/consumo-energia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsumoEnergiaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule // Adicione esta linha aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/consumo-energia/consumo-energia.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consumo-energia',
+  templateUrl: './consumo-energia.component.html',
+  styleUrls: ['./consumo-energia.component.css']
+})
+export class ConsumoEnergiaComponent {
+  title = 'Consumo de Energia';
+  pessoas: number = 0;
+  comodos: number = 0;
+  tvs: number = 0;
+  computadores: number = 0;
+  maquinaLavar: boolean = true;
+  secadora: boolean = true;
+  tarifa: number = 0;
+  consumo: number = 0;
+  valorConta: number = 0;
+
+  calcularConsumo(): void {
+    let consumoComodos = this.comodos * 100;
+    let consumoPessoas = this.pessoas * 120;
+    let consumoTvs = this.tvs * 80;
+    let consumoComputadores = this.computadores * 75;
+    let consumoMaquinaLavar = this.maquinaLavar ? 300 : 0;
+    let consumoSecadora = this.secadora ? 350 : 0;
+
+    this.consumo = consumoComodos + consumoPessoas + consumoTvs + consumoComputadores + consumoMaquinaLavar + consumoSecadora;
+
+    let consumoChuveiro = (5500 * 10) / 60 / 1000;
+    this.consumo += consumoChuveiro;
+
+    this.valorConta = this.consumo * this.tarifa;
+  }
+}
